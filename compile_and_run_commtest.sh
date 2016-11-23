17394dfdb500611ce636b712d07b1330ac3faed7
Elias Rudberg <eliasrudberg@gmail.com>
2016-11-23 22:09:28 +0100

    Changes in compile_and_run_testprog.sh and compile_and_run_commtest.sh to exit when some command fails, to make it easier to see what went wrong.

--- compile_and_run_commtest.sh
+++ compile_and_run_commtest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get ip1 ip1_internal ip2 ip2_internal keyfile by sourcing set_ips_and_keyfile.sh
-. set_ips_and_keyfile.sh
+. set_ips_and_keyfile.sh || exit 1
 
 username=ubuntu
 
@@ -9,16 +9,18 @@ username=ubuntu
 ssh -i $keyfile $username@$ip1 "rm -f commtest.c"
 
 # Copy commtest.c to ip1 host and then compile it
-scp -i $keyfile ../benchmark-codes/communication_test/commtest.c $username@$ip1:
-ssh -i $keyfile $username@$ip1 "rm -f a.out && mpicc -O3 commtest.c && ls -lrt"
+scp -i $keyfile ../benchmark-codes/communication_test/commtest.c $username@$ip1: || exit 1
+ssh -i $keyfile $username@$ip1 "rm -f a.out && mpicc -O3 commtest.c && ls -lrt" || exit 1
 
 # Copy executable a.out to ip2 host
-scp -i $keyfile $username@$ip1:a.out .
-scp -i $keyfile a.out $username@$ip2:
-rm a.out
+scp -i $keyfile $username@$ip1:a.out . || exit 1
+scp -i $keyfile a.out $username@$ip2: || exit 1
+rm a.out || exit 1
 
 echo "Running a.out (commtest.c) with mpirun now..."
-ssh -i $keyfile $username@$ip1 "time mpirun -np 2 -host $ip1_internal,$ip2_internal ./a.out 1000 1 10"
+ssh -i $keyfile $username@$ip1 "time mpirun -np 2 -host $ip1_internal,$ip2_internal ./a.out 1000 1 10" || exit 1
 
 echo "Running a.out (commtest.c) with mpirun now..."
-ssh -i $keyfile $username@$ip1 "time mpirun -np 2 -host $ip1_internal,$ip2_internal ./a.out 10 20000000 1"
+ssh -i $keyfile $username@$ip1 "time mpirun -np 2 -host $ip1_internal,$ip2_internal ./a.out 10 20000000 1" || exit 1
+
+echo ; echo compile_and_run_commtest.sh done.
--- compile_and_run_testprog.sh
+++ compile_and_run_testprog.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 # Get ip1 ip1_internal ip2 ip2_internal keyfile by sourcing set_ips_and_keyfile.sh
-. set_ips_and_keyfile.sh
+. set_ips_and_keyfile.sh || exit 1
 
 username=ubuntu
 
 # Remove any previously existing testprog.c file on ip1 host
-ssh -i $keyfile $username@$ip1 "rm -f testprog.c"
+ssh -i $keyfile $username@$ip1 "rm -f testprog.c" || exit 1
 
 # Copy testprog.c to ip1 host and then compile it
-scp -i $keyfile testprog.c $username@$ip1:
-ssh -i $keyfile $username@$ip1 "mpicc testprog.c && ls -lrt"
+scp -i $keyfile testprog.c $username@$ip1: || exit 1
+ssh -i $keyfile $username@$ip1 "mpicc testprog.c && ls -lrt" || exit 1
 
 # Copy executable a.out to ip2 host
-scp -i $keyfile $username@$ip1:a.out .
-scp -i $keyfile a.out $username@$ip2:
-rm a.out
+scp -i $keyfile $username@$ip1:a.out . || exit 1
+scp -i $keyfile a.out $username@$ip2: || exit 1
+rm a.out || exit 1
 
 echo "Running a.out (from testprog.c) with mpirun now..."
-ssh -i $keyfile $username@$ip1 "mpirun -np 2 -host $ip1_internal,$ip2_internal ./a.out"
+ssh -i $keyfile $username@$ip1 "mpirun -np 2 -host $ip1_internal,$ip2_internal ./a.out" || exit 1
+
+echo ; echo compile_and_run_testprog.sh done.
