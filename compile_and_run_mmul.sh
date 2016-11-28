bb70e3f55ccb4bdebe5653089ceb088397609c4f
Elias Rudberg <eliasrudberg@gmail.com>
2016-11-28 16:11:14 +0100

    Use prepare_openblas.sh in compile_and_run_mmul.sh.

--- compile_and_run_mmul.sh
+++ compile_and_run_mmul.sh
@@ -14,6 +14,9 @@ ssh -i $keyfile $username@$ip1 "rm -rf git_tmp ; mkdir git_tmp ; cd git_tmp ; gi
 # Download and compile CHT-MPI library
 ssh -i $keyfile $username@$ip1 "cd git_tmp/benchmark-codes/mpi_mmul_test ; ./prepare_cht_mpi.sh" || exit 1
 
+# Download and compile OpenBLAS library
+ssh -i $keyfile $username@$ip1 "cd git_tmp/benchmark-codes/mpi_mmul_test ; ./prepare_openblas.sh" || exit 1
+
 # Compile and copy executable files to home directory on ip1 host
 ssh -i $keyfile $username@$ip1 "cd git_tmp/benchmark-codes/mpi_mmul_test && make && cp test_matrix_manager cht_worker ~" || exit 1
 
@@ -24,7 +27,7 @@ scp -i $keyfile test_matrix_manager cht_worker $username@$ip2: || exit 1
 rm test_matrix_manager cht_worker || exit 1
 
 echo "Running test_matrix_manager with mpirun now..."
-ssh -i $keyfile $username@$ip1 "time mpirun -np 1 -host $ip1_internal,$ip2_internal,$ip1_internal,$ip2_internal ./test_matrix_manager 1600 2 > stdout.txt" || exit 1
+ssh -i $keyfile $username@$ip1 "time mpirun -np 1 -host $ip1_internal,$ip2_internal,$ip1_internal,$ip2_internal ./test_matrix_manager 2000 2 > stdout.txt" || exit 1
 
 # Copy result files
 scp -i $keyfile $username@$ip1:stdout.txt . || exit 1
