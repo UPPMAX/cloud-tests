db3126ff68964575511aa9c7af8979856fc8a602
Elias Rudberg <eliasrudberg@gmail.com>
2016-11-28 16:53:36 +0100

    Use 3 input arguments to run_mmul.sh: N nWorkers nThreads.

--- run_mmul.sh
+++ run_mmul.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+if [ "$#" -ne 3 ]; then
+    echo "Please give 3 arguments: N nWorkers nThreads"
+    echo where
+    echo "N         = matrix dimension, should be 1000, 2000, 4000, 8000, 16000 etc"
+    echo "nWorkers  = number of worker processes"
+    echo "nThreads  = number of worker threads used by each worker process"
+    exit 1;
+fi
+
+N=$1
+echo N is $N
+nWorkers=$2
+echo nWorkers is $nWorkers
+nThreads=$3
+echo nThreads is $nThreads
+
 # Get ip1 ip1_internal ip2 ip2_internal keyfile by sourcing set_ips_and_keyfile.sh
 . set_ips_and_keyfile.sh || exit 1
 
@@ -9,10 +25,13 @@ username=ubuntu
 ssh -i $keyfile $username@$ip1 "rm -f stdout.txt output.txt"
 
 echo "Running test_matrix_manager with mpirun now..."
-ssh -i $keyfile $username@$ip1 "mpirun --bind-to none -np 1 -host $ip1_internal,$ip2_internal,$ip1_internal,$ip2_internal ./test_matrix_manager 2000 2 1 > stdout.txt" || exit 1
+ssh -i $keyfile $username@$ip1 "mpirun --bind-to none -np 1 -host $ip1_internal,$ip2_internal,$ip1_internal,$ip2_internal ./test_matrix_manager $N $nWorkers $nThreads > stdout.txt" || exit 1
 
 # Copy result files
 scp -i $keyfile $username@$ip1:stdout.txt . || exit 1
 scp -i $keyfile $username@$ip1:output.txt . || exit 1
+
+echo stdout.txt: ; cat stdout.txt
+grep uteMo output.txt | grep Mul | grep took
 
 echo ; echo run_mmul.sh done.
